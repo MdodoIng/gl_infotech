e665a74bf0d14d3591b4c2d5f9ba01dde41f6c7b
dodo <110053481+MdodoIng@users.noreply.github.com>
2025-04-02 13:05:19 +0530

    modified:   next.config.ts 	modified:   package.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/gl_infotech ",
+  assetPrefix: "/gl_infotech",
 };
 
 export default nextConfig;
